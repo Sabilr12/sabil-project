ec6f9aa50cc9efb0ad7b08e63ed90bbcce64f456
Sabilr12 <sabil23ti@mahasiswa.pcr.ac.id>
2025-05-20 10:57:07 +0700

    zayyan

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ export default function NotFound() {
       <div className="flex items-center justify-center h-screen bg-white text-center">
         <div className="max-w-xl px-4">
           <h1 className="text-8xl font-bold text-gray-800">404</h1>
-          <p className="text-2xl text-gray-600 mt-4">Page Not Found</p>
+          <p className="text-2xl text-gray-600 mt-4"> Page Not Found</p>
         </div>
       </div>
     );
